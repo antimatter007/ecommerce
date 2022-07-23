ce9aa9438e8be5a65dd44423a7af55ea33596249
Ankit Sonal Prasad <66120080+antimatter007@users.noreply.github.com>
2022-07-24 01:36:25 +0530

    Sanity
    Hooked up Sanity with Pages

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-// import { client } from '../lib/client';
+import { client } from '../lib/client';
 import { Product, FooterBanner, HeroBanner } from '../components';
 
 const Home = ({ products, bannerData }) => (
